82cac0a8f5a957ed51b7b04220f3e0ca8dfd3c55
HypixelAdam <mercier200012@hotmail.com>
2018-05-14 15:17:45 -0700

    - Fixed mine auto reset and normal reset to properly reset without replacing only one block

--- PrisonRemade/src/com/elixermc/prison/managers/MineManager.java
+++ PrisonRemade/src/com/elixermc/prison/managers/MineManager.java
@@ -1,7 +1,8 @@
 package com.elixermc.prison.managers;
 
-import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map.Entry;
 import java.util.Random;
 
 import org.bukkit.Bukkit;
@@ -30,10 +31,10 @@ public class MineManager {
 		return loc;
 	}
     public Location getCorner2(String name) {
-    	int x = plugin.getConfig().getInt("Mines."+name+".location.corner1.x");
-		int y = plugin.getConfig().getInt("Mines."+name+".location.corner1.y");
-		int z = plugin.getConfig().getInt("Mines."+name+".location.corner1.z");
-		World w = Bukkit.getWorld(plugin.getConfig().getString("Mines."+name+".location.corner1.world"));
+    	int x = plugin.getConfig().getInt("Mines."+name+".location.corner2.x");
+		int y = plugin.getConfig().getInt("Mines."+name+".location.corner2.y");
+		int z = plugin.getConfig().getInt("Mines."+name+".location.corner2.z");
+		World w = Bukkit.getWorld(plugin.getConfig().getString("Mines."+name+".location.corner2.world"));
 		Location loc = new Location(w,x,y,z);
 		return loc;
 	}
@@ -74,25 +75,33 @@ public class MineManager {
     	}
     	return false;
     }
-    public List<String> getBlockChance(String name) {
+    public HashMap<String, Integer> getBlockChance(String name) {
     	List<String> blocks = plugin.getConfig().getStringList("Mines."+name+".blocks");
-    	List<String> info = new ArrayList<String>();
+    	HashMap<String,Integer> info = new HashMap<String,Integer>();
     	Random ran = new Random();
     	int area = plugin.getConfig().getInt("Mines."+name+".area");
     	for (int count = 0;count<area;count++) {
     		int index = ran.nextInt(blocks.size());
-    		info.add(blocks.get(index));
+    		String block = blocks.get(index);
+    		if (info.containsKey(block)) {
+    			int num = info.get(block);
+    			Bukkit.getConsoleSender().sendMessage(""+num+","+count);
+    			info.put(block,num+1);
+    		} else {
+    			info.put(block,1);
+    		}
     	}
+    	
     	return info;
     }
     @SuppressWarnings("deprecation")
-	public void setBlockType(Location l, Material mat, byte data) {
+	public void setBlockType(Location l, String m, byte d) {
+    	Material mat = Material.getMaterial(Integer.parseInt(m));
     	Block b = l.getBlock();
     	b.setType(mat);
-    	b.setData(data);
+    	b.setData(d);
     	return;
     }
-	@SuppressWarnings("deprecation")
 	public void resetMineAuto(String name) {
 		if (cm == null) {
 			cm = new ConfigManager();
@@ -122,25 +131,33 @@ public class MineManager {
 			for (int y = minY;y <= maxY;y++) {
 				for (int z = minZ;z <= maxZ;z++) {
 					int r = rand.nextInt(100)+1;
-					List<String> info = getBlockChance(name);
+					HashMap<String,Integer> info = getBlockChance(name);
 					Location l = new Location(corner1.getWorld(),x,y,z);
-					for (int i = 0;i<info.size();i++) {
-						String[] split = info.get(i).split(":");
-				        Material mat = Material.getMaterial(Integer.parseInt(split[0]));
-				        byte data = Byte.parseByte(split[1]);
-				        int chance = Integer.parseInt(split[2]);
-				        if (r <= chance) {
-				        	 setBlockType(l,mat,data);
-				        }
-					}
-						
+					for (Entry<String, Integer> set : info.entrySet()) {
+						String[] block = set.getKey().split(":");
+						int count = set.getValue();
+						String mat = block[0];
+						byte data = Byte.parseByte(block[1]);
+						int chance = Integer.parseInt(block[2]);
+						if (count == 0) {
+							info.remove(set.getKey());
+							continue;
+						}
+						if (r <= chance) {
+							setBlockType(l,mat,data);
+							count--;
+							set.setValue(count);
+							break;
+						}
+					}	
 				}
+				continue;
 			}
+			continue;
 		}
 		setResetTimer(name,getResetDelay(name));
 		mm.sendAutoResetMineMessage(name);
 	}
-	@SuppressWarnings("deprecation")
 	public void resetMine(String name) {
 		if (cm == null) {
 			cm = new ConfigManager();
@@ -150,29 +167,32 @@ public class MineManager {
 		}
 		Location corner1 = getCorner1(name);
 		Location corner2 = getCorner2(name);
-		int minX = corner1.getBlockX();
-		int minY = corner1.getBlockY();
-		int minZ = corner1.getBlockZ();
-		int maxX = corner2.getBlockX();
-		int maxY = corner2.getBlockY();
-		int maxZ = corner2.getBlockZ();
+		Bukkit.getConsoleSender().sendMessage(""+corner1.getBlockX()+","+corner1.getBlockY()+","+corner1.getBlockZ()+","+corner1.getWorld().getName());
+		Bukkit.getConsoleSender().sendMessage(""+corner2);
 		Random rand = new Random();
-		for (int x = minX;x <= maxX;x++) {
-			for (int y = minY;y <= maxY;y++) {
-				for (int z = minZ;z <= maxZ;z++) {
+		for (int x = corner1.getBlockX();x <= corner2.getBlockX();x++) {
+			for (int y = corner1.getBlockY();y <= corner2.getBlockY();y++) {
+				for (int z = corner1.getBlockZ();z <= corner2.getBlockZ();z++) {
 					int r = rand.nextInt(100)+1;
-					List<String> info = getBlockChance(name);
+					HashMap<String,Integer> info = getBlockChance(name);
 					Location l = new Location(corner1.getWorld(),x,y,z);
-					for (int i = 0;i<info.size();i++) {
-						String[] split = info.get(i).split(":");
-				        Material mat = Material.getMaterial(Integer.parseInt(split[0]));
-				        byte data = Byte.parseByte(split[1]);
-				        int chance = Integer.parseInt(split[2]);
-				        if (r <= chance) {
-				        	 setBlockType(l,mat,data);
-				        }
+					for (Entry<String, Integer> set : info.entrySet()) {
+						String[] block = set.getKey().split(":");
+						int count = set.getValue();
+						String mat = block[0];
+						byte data = Byte.parseByte(block[1]);
+						int chance = Integer.parseInt(block[2]);
+						if (count == 0) {
+							info.remove(set.getKey());
+							continue;
+						}
+						if (r <= chance) {
+							setBlockType(l,mat,data);
+							count -= count;
+							set.setValue(count);
+							break;
+						}
 					}
-						
 				}
 			}
 		}
